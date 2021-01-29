f7dd30db7e8865e5b03cff63f07287f28009a3e8
LosTacosDeZelda <jeudyraphael@gmail.com>
2021-01-29 09:27:15 -0500

    Premier dépôt de mon projet

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+       <h2>Mon app de fou</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
